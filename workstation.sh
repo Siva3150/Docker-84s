6bf086b0e0b56a637b6f1dcc699619d76e71c221
Siva prasad <sivaprasad3150@gmail.com>
2025-08-02 12:38:09 +0530

    user-profile

--- workstation.sh
+++ workstation.sh
@@ -40,8 +40,12 @@ usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
 
 # eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 mv /tmp/eksctl /usr/local/bin
 eksctl version
 VALIDATE $? "eksctl installation"
